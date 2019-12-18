d5bcd234be11da26a8d28ec41c58c9ee592a424d
ayoakpata <akpata.ayobami@gmail.com>
2019-12-18 19:03:05 +0100

    initial commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return ( 
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+			 	<div className="Tilt-inner">
+			 		<img style={{paddingTop: '5px'}} alt='brainlogo' src={brain}/>
+	 			</div>
+			</Tilt>
+		</div>
+		)
+	}
+
+
+export default Logo;
